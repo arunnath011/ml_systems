0163b8a830f140802249ac23634da6290397e41c
reynard011 <53822804+reynard011@users.noreply.github.com>
2023-01-31 00:15:35 -0500

    modified testing and corrected errors

--- lab_1/src/main.py
+++ lab_1/src/main.py
@@ -1,27 +1,27 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
-
-@app.get("/hello")
-async def hello(name: str):
-    if name is None:
-        raise HTTPException(status_code=400, detail="Name not specified")
-    return {"message": f"Hello {name}"}
 
 @app.get("/")
 async def root():
     raise HTTPException(status_code=501, detail="Not Implemented")
 
+@app.get("/hello")
+async def hello(name: str = None):
+    if name is None:
+        raise HTTPException(status_code=400, detail="Name not specified")
+    return {"message": f"Hello {name}"}
 
 @app.get("/docs")
 async def docs():
-    return {"docs": "API docs here"}
+    return {"message": "browsable while API is running"}
+
 
 @app.get("/openapi.json")
 async def openapi_json():
-    return {"openapi": "3.0.0"}
-
+    return {"openapi_json": "browsable while API is running"}
 
 
 
--- lab_1/tests/test_src.py
+++ lab_1/tests/test_src.py
@@ -8,7 +8,7 @@ def test_hello_endpoint():
     
 def test_hello_endpoint_with_no_name():
     response = requests.get("http://localhost:8000/hello")
-    assert response.status_code == 400
+    assert response.status_code==400
     assert response.json() == {"detail": "Name not specified"}
 
 def test_root_endpoint():
@@ -26,3 +26,40 @@ def test_openapi_endpoint():
     assert "openapi" in response.json()
 
 
+def test_hello_endpoint_with_long_name():
+    long_name = "a" * 250
+    response = requests.get(f"http://localhost:8000/hello?name={long_name}")
+    assert response.status_code == 200
+    assert response.json() == {"message": f"Hello {long_name[:250]}"}
+
+def test_hello_endpoint_with_special_characters_name():
+    special_characters_name = "!@#$%^&*()"
+    response = requests.get(f"http://localhost:8000/hello?name={special_characters_name}")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello !@"}
+
+
+def test_numbers_endpoint():
+    # Test valid input
+    response = requests.get("http://localhost:8000/numbers?num=10")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Not Found"}
+
+    # Test input with decimals
+    response = requests.get("http://localhost:8000/numbers?num=10.5")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Not Found"}
+
+    # Test input with negative numbers
+    response = requests.get("http://localhost:8000/numbers?num=-10")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Not Found"}
+
+    # Test input without number
+    response = requests.get("http://localhost:8000/numbers")
+    assert response.status_code == 404
+    if isinstance(response.json()["detail"], str):
+    	error_message = response.json()["detail"]
+    else:
+    	error_messages = [error_message["msg"] for error_message in response.json()["detail"]]
+    assert "Not Found" in error_message
