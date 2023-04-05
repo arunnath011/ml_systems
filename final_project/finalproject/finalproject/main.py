fdcd0f95589e3e62a8a0d66e5eff5d2d482c55d3
reynard011 <53822804+reynard011@users.noreply.github.com>
2023-04-05 01:51:21 -0400

    adding project files

--- final_project/finalproject/finalproject/main.py
+++ final_project/finalproject/finalproject/main.py
@@ -1,52 +1,58 @@
 import logging
-import os
-
 from fastapi import FastAPI, Request, Response
 from pydantic import BaseModel
+import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
+from transformers import AutoConfig, AutoModelForSequenceClassification, AutoTokenizer
+import json
+import os
+from typing import List
+from fastapi.exceptions import RequestValidationError
+from pydantic import ValidationError
 
-model_path = "./distilbert-base-uncased-finetuned-sst2"
-model = AutoModelForSequenceClassification.from_pretrained(model_path)
-tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+app = FastAPI()
+
+hub_path = "./distilbert-base-uncased-finetuned-sst2"
+
+model = AutoModelForSequenceClassification.from_pretrained(hub_path)
+tokenizer = AutoTokenizer.from_pretrained(hub_path)
+
 classifier = pipeline(
     task="text-classification",
     model=model,
     tokenizer=tokenizer,
     device=-1,
-    return_all_scores=True,
+    top_k=None,
 )
-
-#pydantic models
-
-
-logger = logging.getLogger(__name__)
-LOCAL_REDIS_URL = "redis://redis:6379"
-app = FastAPI()
-
-
-@app.on_event("startup")
-def startup():
-    pass
-
 
 class SentimentRequest(BaseModel):
     text: List[str]
-
 
 class Sentiment(BaseModel):
     label: str
     score: float
 
-
 class SentimentResponse(BaseModel):
     predictions: List[Sentiment]
-
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 @app.post("/predict", response_model=SentimentResponse)
 def predict(sentiments: SentimentRequest):
     predictions = classifier(sentiments.text)
     response = SentimentResponse(
-        predictions=[Sentiment(label=label, score=score) for pred in predictions for label, score in pred["scores"]])
+        predictions=[
+            Sentiment(
+                label=label,
+                score=score
+            )
+            for pred in predictions
+            for label, score in zip(
+                [p.get("label") for p in pred],
+                [p.get("score") for p in pred]
+            )
+        ]
+    )
+
     return response
 
 @app.get("/health")
